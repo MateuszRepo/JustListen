b7883eb05df29de4075ae11d949b6fc1fb76cb42
Mateusz <mateuszbe1993@o2.pl>
2020-11-27 19:34:53 +0100

    Firebase Music Source finished

--- app/src/main/java/com/mateuszb/justlisten/di/ServiceModule.kt
+++ app/src/main/java/com/mateuszb/justlisten/di/ServiceModule.kt
@@ -6,6 +6,7 @@ import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import com.mateuszb.justlisten.data.remote.MusicDatabaseFirebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,6 +18,10 @@ import javax.inject.Singleton
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideMusicDatabase() = MusicDatabaseFirebase()
 
     @ServiceScoped
     @Provides
