f4c151f690ea54cee206a70779b69c16821bf539
Mateusz <mateuszbe1993@o2.pl>
2020-11-28 17:51:56 +0100

    Setting up Music Notification

--- app/src/main/java/com/mateuszb/justlisten/other/Constants.kt
+++ app/src/main/java/com/mateuszb/justlisten/other/Constants.kt
@@ -3,4 +3,8 @@ package com.mateuszb.justlisten.other
 object Constants {
 
     const val SONGS_COLLECTION = "songs"
+
+
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
 }
